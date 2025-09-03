409cea64090d6075e1bfa60bf7f765696ab736c4
Behruzbek0771 <behruzbekqalandarov771@gmail.com>
2025-09-03 10:03:33 +0500

    tasklar

---
+++ task01.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, brand, model, year ):
+        self.brand = brand
+        self.model = model
+        self.year = year
+    
+    def get_info (self):
+        return f"brand: {self.brand}, model: {self.model}, year: {self.year}"
+    
+car = Car("BMW", "X5", 2020)
+print(car.get_info())
